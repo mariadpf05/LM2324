76902e3332188ef2c70e82a896102a0a1ee075cb
mariadpf05 <mariadelpino05@iesamachado.org>
2024-01-12 09:17:31 +0100

    A

--- 2ºTrimestre/javascript/circulos.js
+++ 2ºTrimestre/javascript/circulos.js
@@ -1,25 +1,26 @@
-/*  Versión cutre
+/* 
+Versión cutre 
 function ocultarRojo(){
-    document.getElementById("rojo").style.visibility="hidden"
+    document.getElementById("rojo").style.visibility = "hidden";
 }
 function ocultarVerde(){
-    document.getElementById("verde").style.visibility="hidden"    
+    document.getElementById("verde").style.visibility = "hidden";
 }
 function ocultarAzul(){
-    document.getElementById("azul").style.visibility="hidden"
+    document.getElementById("azul").style.visibility = "hidden";
 }
 */
-/*Versión mejorada*/
-function ocultar(caja){
-    document.getElementById(caja).style.visibility = "hidden";
+/* Versión mejorada */
+function ocultar(x){
+    document.getElementById(x).style.visibility = "hidden";
 }
 function mostrar(caja){
     document.getElementById(caja).style.visibility = "visible";
 }
-/*Versión con un solo botón*/
-function ocultar_o_mostrar (caja){
-    if (document.getElementById(caja).style.visibility == "visible")
+/* Versión con un solo boton */
+function ocultar_o_mostrar(caja){
+    if (document.getElementById(caja).style.visibility =="visible" || document.getElementById(caja).style.visibility =="")
         document.getElementById(caja).style.visibility = "hidden";
-    else 
-        document.getElementById(caja).style.visibility == "visible";
-} 
+    else
+        document.getElementById(caja).style.visibility = "visible";
+}
