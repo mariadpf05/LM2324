24a05110fe436f1892320c0cd01f78177ccb423a
mariadpf05 <mariadelpino05@iesamachado.org>
2024-01-12 14:23:06 +0100

    aeiou

---
+++ 2ºTrimestre/javascript/c-grados.js
@@ -0,0 +1,15 @@
+/*Código javascript del conversor*/
+function convertir(divisa){
+    let valor = document.getElementById("grados").value;
+    let resultado = 0;
+    if (divisa=="Celcius"){
+        //Convierto a celcius
+        resultado = valor * 0.91;
+        document.getElementById("resultado").innerHTML = resultado + " €";
+    }
+    else{
+        //Convertir a Fahrenheit
+        resultado = valor * 1.09;
+        document.getElementById("resultado").innerHTML = resultado + " $";
+    }
+}
