fc7ce80d3272b9566c4f9ba71ecb555307ae7fb1
mariadpf05 <mariadelpino05@iesamachado.org>
2024-01-22 07:50:08 +0100

    a

---
+++ 2ºTrimestre/javascript/arraysejercicio.js
@@ -0,0 +1,10 @@
+var frutas = ["Platano", "Naranja", "Manzana", "Mango"];
+document.getElementById("demo").innerHTML = frutas;
+function elimina() {
+frutas.pop();
+document.getElementById("demo").innerHTML = frutas;
+}
+function añade() {
+frutas.push();
+document.getElementById("demo").innerHTML = frutas;
+}
