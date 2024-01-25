7197dd577645bd58af0a2a1e9e4d3e6139425a9d
mariadpf05 <mariadelpino05@iesamachado.org>
2024-01-25 13:06:49 +0100

    cambios

--- 2ºTrimestre/javascript/bienvenido.js
+++ 2ºTrimestre/javascript/bienvenido.js
@@ -4,4 +4,11 @@ function saludo() {
     var clave;
     clave = document.getElementById("clave").value;
     alert('Bienvenido '+usuario + ' con contraseña ' + clave);
+/*
+    if(usuario =="" || password== ""){
+    alert('Introduzca usuario y contraseña');
     }
+    else {
+        alert('Bienvenido '+usuario + ' con contraseña ' + clave);
+    } */
+}
