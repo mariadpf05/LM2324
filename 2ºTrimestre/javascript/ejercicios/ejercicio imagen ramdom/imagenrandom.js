7c90236e1a9c7e80c5cc385712866005de4cbfe4
mariadpf05 <mariadelpino05@iesamachado.org>
2024-01-30 13:10:31 +0100

    aleatorio

--- 2ºTrimestre/javascript/ejercicios/ejercicio imagen ramdom/imagenrandom.js
+++ 2ºTrimestre/javascript/ejercicios/ejercicio imagen ramdom/imagenrandom.js
@@ -1,9 +1,6 @@
-function rand(n){
-    // creamos un numero al azar entre 1 y 10 (o cual sea la cantidad de imágenes)
-     return(Math.floor(Math.random() * n + 1 ));
+function numero(x){
+     return(Math.floor(Math.random() * x + 1 ));
     }
-
-    //guardas imagenes en el array
     var cambia_imagen = new Array();
     cambia_imagen[0] = "imagen1.jpg";
     cambia_imagen[1] = "imagen2.jpg";
@@ -13,10 +10,6 @@ function rand(n){
     cambia_imagen[5] = "imagen6.jpg";
     cambia_imagen[6] = "imagen7.jpg";
 
-    //la función para que al clickear establezca el source del tag imagem que tiene id "ia" (Imagen aleatoria)
-    //como no son tantas, puede que alguna vez se repita 2 veces la misma
-    //incluso, si usamos numeros para las imágenes, la script puede ser más sencilla
-    
-    function aleatoria(){
-         document.getElementById("aleatoria").src = cambia_imagen[rand(7)-1];
-    }
+function aleatoria(){
+     document.getElementById("aleatoria").src = cambia_imagen[numero(7)-1];
+}
