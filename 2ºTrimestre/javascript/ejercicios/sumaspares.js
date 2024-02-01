eff726cdbbb04d3f68b4c7dfc3784436e2a40334
mariadpf05 <mariadelpino05@iesamachado.org>
2024-02-01 13:24:33 +0100

    cambios

--- 2ºTrimestre/javascript/ejercicios/sumaspares.js
+++ 2ºTrimestre/javascript/ejercicios/sumaspares.js
@@ -1,3 +1,16 @@
+let numeros=[1,2,3,4,5,6,7,8,9,10];
+
+function sumaPares(numeros){
+    let total = 0.0;
+    for (var i=0;i<numeros.length;i++){
+       if (numeros[i]%2 == 0.0)
+        total = total + numeros[i];
+    }
+    document.getElementById("resultado").innerHTML = total;
+}
+    sumaPares(numeros);
+
+/* sumar pares añadiendo tú los números
 var numeros = [ ];
 function añadir(){
     var n = parseFloat(document.getElementById("n").value);
@@ -5,13 +18,13 @@ function añadir(){
     document.getElementById("resultado").innerHTML = numeros 
 }
 var s = 0
-function suma(){
+function sumarpares(){
     s = 0 
     for (var i = 0 ; i < numeros.length; i++) {
         if (numeros[i]%2 == 0){
             s = s + numeros[i]
-        }
-        
+        } 
     }
     document.getElementById("resultado").innerHTML = s
 }
+*/
