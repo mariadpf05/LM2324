27c59629939d8b13c05a372545d67bd155a9a1ff
mariadpf05 <mariadelpino05@iesamachado.org>
2024-02-02 11:36:06 +0100

    CAMBIOS

--- 2ºTrimestre/javascript/validar.js
+++ 2ºTrimestre/javascript/validar.js
@@ -1,11 +1,30 @@
 function validar(elementos){
     let estanCorrectos = true;
     for (var i=0;i<elementos.length;i++){
-        if (elementos[i].value ==""){
-            document.getElementById("campo"+(i+1).toString).innerHTML = "El campo " + elementos[i].id + "esta vacio"
-    
+        document.getElementById("campo"+(i+1).toString()).innerHTML = "";        
+        if (elementos[i].value == "" || (i==4 && !elementos[i].checked)){
+            document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " está vacío";
             estanCorrectos = false;
         }
+        
+    }
+    if (!validarEmail()){
+        document.getElementById("campo3").innerHTML = "Email no válido";        
+        estanCorrectos = false;
     }
     return estanCorrectos;
 }
+/* Función validarEmail tomada de:
+* https://www.coderbox.net/blog/validar-email-usando-javascript-y-expresiones-regulares/
+*/
+function validarEmail(){              
+	var valido;
+	var emailField = document.getElementById('email');
+	var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+	if( validEmail.test(emailField.value) ){
+		valido=true;
+	}else{
+        valido=false;
+	}
+    return valido;
+} 
