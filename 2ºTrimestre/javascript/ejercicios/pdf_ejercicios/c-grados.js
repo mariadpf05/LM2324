02ec76c23e0e4f77ea223de9b2abd4a4edcf876e
mariadpf05 <145689212+mariadpf05@users.noreply.github.com>
2024-03-14 18:34:29 +0100

    comentarios

--- 2ºTrimestre/javascript/ejercicios/pdf_ejercicios/c-grados.js
+++ 2ºTrimestre/javascript/ejercicios/pdf_ejercicios/c-grados.js
@@ -1,15 +1,15 @@
 /*Código javascript del conversor*/
 function convertir(divisa){
-    let valor = document.getElementById("grados").value;
-    let resultado = 0;
-    if (divisa=="Celcius"){
+    let valor = document.getElementById("grados").value;  // a la variable valor de damos el valor de los grados introducidos
+    let resultado = 0;  //Valor inicial 0
+    if (divisa=="Celcius"){  //ºF a ºC
         //Convierto a celcius
-        resultado = (valor - 32) * 5/9;
-        document.getElementById("resultado").innerHTML = resultado + " ºC";
+        resultado = (valor - 32) * 5/9;  //resultado pasa a ser el cambio de grados
+        document.getElementById("resultado").innerHTML = resultado + " ºC";  //Se mostrara en el parrafo resultado el valor calculado antes
     }
     else{
         //Convertir a Fahrenheit
-        resultado = (valor * 9/5) + 32;
-        document.getElementById("resultado").innerHTML = resultado + " ºF";
+        resultado = (valor * 9/5) + 32;  //resultado pasa a ser el cambio de grados
+        document.getElementById("resultado").innerHTML = resultado + " ºF";  //Se mostrara en el parrafo resultado el valor calculado antes
     }
 }
--- 2ºTrimestre/javascript/ejercicios/pdf_ejercicios/conversor.js
+++ 2ºTrimestre/javascript/ejercicios/pdf_ejercicios/conversor.js
@@ -1,8 +1,8 @@
 /*Código javascript del conversor*/
 function convertir(divisa){
     let valor = document.getElementById("cantidad").value;  // a la variable valor de damos el valor de la cantidad introducida
-    let resultado = 0;  //Establecemos el resultado en 0 al principio
-    if (divisa=="euro"){ //Si la moneda es €
+    let resultado = 0;  //Establecemos el valor inicial en 0 al principio
+    if (divisa=="euro"){  //$ a €
         //Convierto a euro
         resultado = valor * 0.91;  //El resultado sera la cantidad * el cambio
         document.getElementById("resultado").innerHTML = resultado + " €";  //En el parrafo se mostrará el resultado + €
